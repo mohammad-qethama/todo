1f8ebdbde91875192dbb282ea6f50206c78eb8e7
mohammad-qethama <mhmd.a@protonmail.com>
2021-06-28 12:14:27 +0300

    ghpages

---
+++ build/precache-manifest.0b5a5b0da80b45587edf85c9073c8e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443c133743c451f39f1f1c29e3128f18",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "47346af27ebb16bd4487",
+    "url": "/todo/static/css/main.fe9910a7.chunk.css"
+  },
+  {
+    "revision": "8c4aa5671c5ef6244dc7",
+    "url": "/todo/static/js/2.8365c248.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/todo/static/js/2.8365c248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47346af27ebb16bd4487",
+    "url": "/todo/static/js/main.de165169.chunk.js"
+  },
+  {
+    "revision": "4b7c96a078d3292937b1",
+    "url": "/todo/static/js/runtime-main.d4ce9cd5.js"
+  }
+]);
